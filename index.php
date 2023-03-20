a2dfa8cb450f89d896cbc196250faf47ae3e74e5
bleenie <117716131+bleenie@users.noreply.github.com>
2023-03-20 11:27:23 +0100

    update

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/21918895c4.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="header-left"></div>
+        <div class="header-right">
+            <a href=""><i class="fa-regular fa-user fa-xs"></i> LOGIN</a>
+            <a href="">CONTACT</a>
+            <a href="">MENU</a>
+            <button class="cart-btn"><i class="fa-solid fa-cart-shopping fa-sm"></i> CART</button>
+        </div>
+    </header>
+    <img class="banner" src="./images/banner.png" alt="banner">
+    <main>
+        <div class="about">
+            <div class="about-text">
+                <h2>OVER ONS</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam scelerisque, sapien in pellentesque posuere, lorem augue suscipit enim, in venenatis felis tellus ac tortor. Nunc a risus faucibus, sollicitudin lorem in, pharetra justo. Sed fringilla tincidunt ex. Duis in gravida diam. Donec accumsan aliquam nunc eu posuere. In hac habitasse platea dictumst. Nunc pharetra massa sit amet nulla ultrices, dictum hendrerit est venenatis. Donec quis venenatis justo.</p>
+            </div>
+            <div class="about-img">
+                <img src="./images/about.jpg" alt="about">
+            </div>
+        </div>
+        <h1>ONS MENU</h1>
+        <div class="menu">
+            <div class="menu-search"></div>
+            <div class="menu-title">
+                <h2>POPULAIRE GERECHTEN</h2>
+            </div>
+            <div class="menu-item-box">
+                <div class ="menu-item"></div>
+                <div class ="menu-item"></div>
+                <div class ="menu-item"></div>
+                <div class ="menu-item"></div>
+                <div class ="menu-item"></div>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
